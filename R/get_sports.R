821f9583b22b827b09ab5f0e01d988750dbc6585
JaseZiv <jaseziv83@gmail.com>
2022-09-11 20:26:17 +1000

    first commit

---
+++ R/get_sports.R
@@ -0,0 +1,34 @@
+require(httr)
+library(jsonlite)
+library(dplyr)
+
+
+params = list(
+  `jurisdiction` = 'VIC'
+)
+
+res <- httr::GET(url = 'https://api.beta.tab.com.au/v1/tab-info-service/sports', query = params)
+
+a <- httr::content(res)
+b <- a$sports
+
+all_data <- data.frame()
+
+for(i in b) {
+  each_sport <- jsonlite::toJSON(i) %>% jsonlite::fromJSON() %>% data.frame()
+
+  all_data <- dplyr::bind_rows(all_data, each_sport)
+}
+
+all_data <- all_data %>%
+  dplyr::select(-competitions.tournaments)
+
+all_data <- tidyr::unnest(all_data,
+                     cols = c(competitions.id, competitions.name, competitions.spectrumId,
+                              competitions._links, competitions.hasMarkets,
+                              competitions.sameGame))
+
+saveRDS(all_data, "data/sports_markets.rds")
+
+
+
---
+++ R/race-meets/get_race_meets_incremental.R
@@ -0,0 +1,102 @@
+library(httr)
+library(rvest)
+library(lubridate)
+library(tidyverse)
+
+
+.replace_empty_na <- function(val) {
+  if(length(val) == 0) {
+    val <- NA_character_
+  } else {
+    val <- val
+  }
+  return(val)
+}
+
+
+.each_race_date <- function(each_date) {
+
+  each_url <- paste0('https://api.beta.tab.com.au/v1/historical-results-service/VIC/racing/', each_date)
+
+  history <- httr::RETRY("GET",
+                         url = each_url,
+                         times = 5, # the function has other params to tweak its behavior
+                         pause_min = 5,
+                         pause_base = 2)
+
+  history <- history %>% httr::content()
+
+  # need a while loop here as there were still times when the API was failing and returning a list of length zero
+  # have arbitrarily set the max number of retries in the while-loop to 20 - might want to parameterise this later
+  iter <- 1
+  while(length(history) == 0) {
+
+    iter <- iter + 1
+    stopifnot("The API is not accepting this request. Please try again." = iter <21)
+
+    Sys.sleep(2)
+
+    history <- httr::RETRY("GET",
+                           url = each_url,
+                           times = 5, # the function has other params to tweak its behavior
+                           pause_min = 5,
+                           pause_base = 2)
+
+    history <- history %>% httr::content()
+
+  }
+
+  meetings_list <- history$meetings
+
+
+  .each_race_meet <- function(x) {
+    meta <- data.frame(meetingName = tryCatch(as.character(x[["meetingName"]]) %>% .replace_empty_na(), error = function(e) NA_character_),
+                       location = tryCatch(as.character(x[["location"]]) %>% .replace_empty_na(), error = function(e) NA_character_),
+                       venueMnemonic = tryCatch(as.character(x[["venueMnemonic"]]) %>% .replace_empty_na(), error = function(e) NA_character_),
+                       raceType = tryCatch(as.character(x[["raceType"]]) %>% .replace_empty_na(), error = function(e) NA_character_),
+                       meetingDate = tryCatch(as.character(x[["meetingDate"]]) %>% .replace_empty_na(), error = function(e) NA_character_),
+                       weatherCondition = tryCatch(as.character(x[["weatherCondition"]]) %>% .replace_empty_na(), error = function(e) NA_character_),
+                       trackCondition = tryCatch(as.character(x[["trackCondition"]]) %>% .replace_empty_na(), error = function(e) NA_character_),
+                       meetUrl = tryCatch(as.character(x[["_links"]][["self"]]) %>% .replace_empty_na(), error = function(e) NA_character_),
+                       numRaces = tryCatch(x$races %>% length() %>% .replace_empty_na(), error = function(e) NA_character_)
+    )
+  }
+
+  dat <- meetings_list %>%
+    purrr::map_df(.each_race_meet)
+
+}
+
+
+
+
+get_race_meet_meta <- function(race_dates) {
+
+  race_dates %>%
+    purrr::map_df(.each_race_date)
+}
+
+
+
+existing_df <- readRDS("data/race_meets_21_22.rds")
+
+# now we want to get updated data - take te day after the latest last scrape, up to the day before today
+# (as typically there won't be today's race meet data available)
+dates <- seq(from = lubridate::ymd(max(existing_df$meetingDate))+1, to=lubridate::today()-1, by=1) %>% as.character()
+
+
+# Start scraping loop -----------------------------------------------------
+
+race_meets <- data.frame()
+
+
+for(i in dates) {
+  print(paste("scraping date:", i))
+  df <- get_race_meet_meta(i)
+  race_meets <- dplyr::bind_rows(race_meets, df)
+}
+
+
+saveRDS(race_meets, "data/race_meets_21_22.rds")
+
+
