e66d8d647c5877d0fc9f821d2fabbedff8155550
Jason Zivkovic <Jason.Zivkovic@reece.com.au>
2022-10-08 06:57:19 +1100

    test switch to releases for nbl data

--- R/nbl_betting.R
+++ R/nbl_betting.R
@@ -1,20 +1,48 @@
 library(dplyr)
 library(httr)
 library(bettRtab)
+library(piggyback)
 
-# NFL Futures -------------------------------------------------------------
 
 Sys.setenv(TZ = "Australia/Melbourne")
 
 scrape_date <- Sys.Date()
-
-nbl_betting <- readRDS(file.path("data", "nbl_betting.rds"))
 
 httr::set_config(httr::use_proxy(url = Sys.getenv("PROXY_URL"),
                                  port = as.numeric(Sys.getenv("PROXY_PORT")),
                                  username =Sys.getenv("PROXY_USERNAME"),
                                  password= Sys.getenv("PROXY_PASSWORD")))
 
+# Save to and read from releases -------------------------------------------
+
+save_to_release <- function(df, file_name, release_tag) {
+
+  temp_dir <- tempdir(check = TRUE)
+  .f_name <- paste0(file_name,".rds")
+  saveRDS(df, file.path(temp_dir, .f_name))
+
+  piggyback::pb_upload(file.path(temp_dir, .f_name),
+                       repo = "JaseZiv/Betting-Data",
+                       tag = release_tag
+  )
+
+}
+
+
+file_reader <- function(file_name, release_tag) {
+  f_name <- paste0(file_name, ".rds")
+  piggyback::pb_download(f_name,
+                         tag = release_tag,
+                         dest = tempdir())
+
+  readRDS(f_name)
+}
+
+
+# Function to get data ----------------------------------------------------
+
+# nbl_betting <- readRDS(file.path("data", "nbl_betting.rds"))
+nbl_betting <- file_reader("nbl_betting", "sports")
 
 nbl_markets <- tryCatch(get_sports_market(competition_name = "NBL"), error = function(e) data.frame())
 futures_markets <- tryCatch(get_sports_market(competition_name = "NBL Futures"), error = function(e) data.frame())
@@ -25,8 +53,10 @@ futures_markets$scrape_date <- scrape_date
 
 futures_markets <- dplyr::bind_rows(futures_markets, nbl_betting)
 
+# saveRDS(futures_markets, "data/nbl_betting.rds")
 
-saveRDS(futures_markets, "data/nbl_betting.rds")
+save_to_release(nbl_betting, "nbl_betting", "sports")
+
 
 
 rm(list = ls())
