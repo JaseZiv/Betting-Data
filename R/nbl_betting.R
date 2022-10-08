b2c326d3ede66684d489fd5807c2ea4936c28d3a
Jason Zivkovic <Jason.Zivkovic@reece.com.au>
2022-10-08 16:58:42 +1100

    update data manually

--- R/nbl_betting.R
+++ R/nbl_betting.R
@@ -13,36 +13,36 @@ httr::set_config(httr::use_proxy(url = Sys.getenv("PROXY_URL"),
                                  username =Sys.getenv("PROXY_USERNAME"),
                                  password= Sys.getenv("PROXY_PASSWORD")))
 
-# Save to and read from releases -------------------------------------------
-
-save_to_release <- function(df, file_name, release_tag) {
-
-  temp_dir <- tempdir(check = TRUE)
-  .f_name <- paste0(file_name,".rds")
-  saveRDS(df, file.path(temp_dir, .f_name))
-
-  piggyback::pb_upload(file.path(temp_dir, .f_name),
-                       repo = "JaseZiv/Betting-Data",
-                       tag = release_tag
-  )
-
-}
-
-
-file_reader <- function(file_name, release_tag) {
-  f_name <- paste0(file_name, ".rds")
-  piggyback::pb_download(f_name,
-                         tag = release_tag,
-                         dest = tempdir())
-
-  readRDS(f_name)
-}
+# # Save to and read from releases -------------------------------------------
+#
+# save_to_release <- function(df, file_name, release_tag) {
+#
+#   temp_dir <- tempdir(check = TRUE)
+#   .f_name <- paste0(file_name,".rds")
+#   saveRDS(df, file.path(temp_dir, .f_name))
+#
+#   piggyback::pb_upload(file.path(temp_dir, .f_name),
+#                        repo = "JaseZiv/Betting-Data",
+#                        tag = release_tag
+#   )
+#
+# }
+#
+#
+# file_reader <- function(file_name, release_tag) {
+#   f_name <- paste0(file_name, ".rds")
+#   piggyback::pb_download(f_name,
+#                          tag = release_tag,
+#                          dest = tempdir())
+#
+#   readRDS(f_name)
+# }
 
 
 # Function to get data ----------------------------------------------------
 
-# nbl_betting <- readRDS(file.path("data", "nbl_betting.rds"))
-nbl_betting <- file_reader("nbl_betting", "sports")
+nbl_betting <- readRDS(file.path("data", "nbl_betting.rds"))
+# nbl_betting <- file_reader("nbl_betting", "sports")
 
 nbl_markets <- tryCatch(get_sports_market(competition_name = "NBL"), error = function(e) data.frame())
 futures_markets <- tryCatch(get_sports_market(competition_name = "NBL Futures"), error = function(e) data.frame())
@@ -53,9 +53,9 @@ futures_markets$scrape_date <- scrape_date
 
 futures_markets <- dplyr::bind_rows(futures_markets, nbl_betting)
 
-# saveRDS(futures_markets, "data/nbl_betting.rds")
+saveRDS(futures_markets, "data/nbl_betting.rds")
 
-save_to_release(nbl_betting, "nbl_betting", "sports")
+# save_to_release(nbl_betting, "nbl_betting", "sports")
 
 
 
